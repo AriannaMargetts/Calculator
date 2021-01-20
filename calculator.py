3fea2c06c1b410c08eae87d0489fb3d241ecb553
Arianna Margetts <61380509+AriannaMargetts@users.noreply.github.com>
2021-01-20 21:53:55 +0000

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def calculator(first_num, second_num, operation):
+    
+    if operation == "+":
+        return first_num + second_num
+
+    elif operation == "-":
+        return first_num - second_num
+
+    elif operation == "/":
+        return first_num / second_num
+
+    elif operation == "*":
+        return first_num * second_num
+
+    elif operation == "^":
+        return first_num ** second_num
+
+    else:
+        print("You're stoopid try again")
+        exit()
+
+first_num = float(input("Pick first number: "))
+second_num = float(input("Pick second number: "))
+operation = input("Pick an operation: +, -, *, / or ^: ")
+print("The answer is: " + str(calculator(first_num, second_num, operation)))
